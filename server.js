0ac3f948e542ef349175fe75eebfc69b8be58161
Thomas <t.w.youens2@newcastle.ac.uk>
2023-06-19 16:29:31 +0100

    add public routers

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const app = express();
 var path = require('path');
 require('dotenv').config()
 const manageRoutes = require('./src/routes/manage')
+const publicRoutes = require('./src/routes/public')
 
 app.set('views', path.join(__dirname, 'src/views')); 
 app.set('view engine', 'ejs');
 
 app.use('/manage', manageRoutes)
+app.use('/', publicRoutes)
 
 app.listen(process.env.APP_PORT, ()=>{console.log(`Server started! vist: http://${process.env.APP_HOST}:${process.env.APP_PORT}`)})
