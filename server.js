29dbfb1d1ae5592b23dba51efa374fa8e89c7431
Thomas <t.w.youens2@newcastle.ac.uk>
2023-06-19 17:04:44 +0100

    add db connection

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ var path = require('path');
 require('dotenv').config()
 const manageRoutes = require('./src/routes/manage')
 const publicRoutes = require('./src/routes/public')
+const connect = require('./src/services/db.service');
 
 app.set('views', path.join(__dirname, 'src/views')); 
 app.set('view engine', 'ejs');
 
 app.use('/manage', manageRoutes)
 app.use('/', publicRoutes)
-
+connect();
 app.listen(process.env.APP_PORT, ()=>{console.log(`Server started! vist: http://${process.env.APP_HOST}:${process.env.APP_PORT}`)})
