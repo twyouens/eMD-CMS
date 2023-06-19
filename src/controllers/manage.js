90f1da1aa8d1edb0a60d86fb0bc3587fc310ca0a
Thomas <t.w.youens2@newcastle.ac.uk>
2023-06-19 18:00:24 +0100

    added new page and addpage functions

--- src/controllers/manage.js
+++ src/controllers/manage.js
@@ -1,18 +1,26 @@
+const PageModel  = require('../models/page');
+
 function main(req, res, next) {
     res.render('pages/manage/index')
 }
 
 async function pages (req, res, next) {
-    const page = {
-        slug: "about-us",
-        title: "About Us",
-        createdAt: "20/05/2023"
-    }
-    res.render('pages/manage/pages', { pages: [page] })
+    const pages = await PageModel.find().sort({ createdAt: 'desc' })
+    res.render('pages/manage/pages', { pages: pages })
+}
+
+async function addPage(req, res, next) {
+    newAsset = await Asset.create(req.body)
+}
+
+async function newPage(req, res, next) {
+    res.render('pages/manage/newPage')
 }
 
 
 module.exports = {
     main,
-    pages
+    pages,
+    newPage,
+    addPage
 }
