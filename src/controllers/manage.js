30b5892eb16e8c173dc3f666143d37dd5314476a
Thomas <t.w.youens2@newcastle.ac.uk>
2023-06-19 16:32:15 +0100

    added test pages controller function

--- src/controllers/manage.js
+++ src/controllers/manage.js
@@ -2,6 +2,17 @@ function main(req, res, next) {
     res.render('pages/manage/index')
 }
 
+async function pages (req, res, next) {
+    const page = {
+        slug: "about-us",
+        title: "About Us",
+        createdAt: "20/05/2023"
+    }
+    res.render('pages/manage/pages', { pages: [page] })
+}
+
+
 module.exports = {
-    main
+    main,
+    pages
 }
