c19273f893d2e45b62ce7972203fa2068743821b
Thomas <t.w.youens2@newcastle.ac.uk>
2023-06-20 16:57:06 +0100

    added auth handling

--- src/controllers/public.js
+++ src/controllers/public.js
@@ -1,3 +1,7 @@
+const authentication = require('../middleware/auth')
+const UserModel = require('../models/user')
+var crypto = require('crypto');
+
 function home(req, res, next) {
     res.send("Home")
 }
@@ -10,8 +14,38 @@ function signIn(req, res, next) {
     res.render("pages/public/signin")
 }
 
+async function registerUser(req, res, next){
+    const salt = crypto.randomBytes(16).toString('hex');
+    const hash = crypto.pbkdf2Sync(req.body.password, salt, 10000, 512, 'sha512').toString('hex');
+    try{
+        newUser = await UserModel.create({username: req.body.username,name: req.body.name, hash: hash, salt: salt})
+        console.log(newUser)
+        res.redirect('/sign-in')
+    }catch(error){
+        res.send("Error")
+        console.error(error.message)
+    }
+}
+
+async function logout(req, res, next) {
+    res.clearCookie('jwt');
+    res.redirect('/'); // Redirect to the desired page after logout
+}
+
+
+signInPost = [
+    authentication.authenticate,
+    (req, res) => {
+        // Redirect or respond with success message
+        res.redirect('/manage');
+    },
+];
+
 module.exports = {
     home,
     page,
-    signIn
+    signIn,
+    signInPost,
+    logout,
+    registerUser
 }
