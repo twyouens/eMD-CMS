e471ee569df1fb3415e3ffca9607e23863f5d832
Thomas <t.w.youens2@newcastle.ac.uk>
2023-06-20 10:48:40 +0100

    added addPage and edit page functions

--- src/controllers/manage.js
+++ src/controllers/manage.js
@@ -10,11 +10,23 @@ async function pages (req, res, next) {
 }
 
 async function addPage(req, res, next) {
-    newAsset = await Asset.create(req.body)
+    try{
+        const newPage = await PageModel.create(req.body)
+        res.redirect(`pages/${newPage.id}`)
+    } catch(error){
+        console.error("Error when trying to create page",error.message)
+        res.redirect('pages/new')
+    }
 }
 
 async function newPage(req, res, next) {
     res.render('pages/manage/newPage')
+}
+
+async function editPage(req, res, next) {
+    const page = await PageModel.findById(req.params.id)
+    if (page == null) res.redirect('/manage/pages')
+    res.json(page)
 }
 
 
@@ -22,5 +34,6 @@ module.exports = {
     main,
     pages,
     newPage,
-    addPage
+    addPage,
+    editPage
 }
